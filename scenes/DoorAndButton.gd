33350b0cb242a00df5b160f18ff857d82da0c7d0
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-08-16 14:57:28 +1200

    vid9

---
+++ scenes/DoorAndButton.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _ready():
+	$AnimationPlayer.play("ButtonUp")
+	$AnimationPlayer.play("DoorClosed")
+	
+
+
+
+func _on_Area2D_body_entered(body):
+	if "Player" in str(body):
+		$AnimationPlayer.play("ButtonDown")
+		$AnimationPlayer.play("DoorOpen")
+		
+
