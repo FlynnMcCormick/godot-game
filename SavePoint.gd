19b8235210b0f80d15c9d999103b1b0c8420fe32
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-09-05 09:41:06 +1200

    vid10_13.58

--- SavePoint.gd
+++ SavePoint.gd
@@ -11,7 +11,10 @@ func _on_SavePoint_body_entered(body):
 	if "Player" in str(body):
 		GameStats.set_spawn(self)
 		$AnimationPlayer.play("saved")
-				
+		if body.is_in_group("Player"):
+			print("Player Dead")
+			if GameStats.check_reset() == false:
+				body.global_position = GameStats.get_spawn().global_position
 
 
 
--- scenes/SpikeTrap.gd
+++ scenes/SpikeTrap.gd
@@ -7,3 +7,4 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if "Player" in str(body):
 		print("playerdead")
+
