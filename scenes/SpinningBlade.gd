5437143693706c245145e3d02c8c0664bb3a499a
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-10-19 15:03:22 +1300

    4 mins

--- scenes/SpinningBlade.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export (int) var follow_speed = 50
-
-func _ready():
-	$AnimationPlayer.play ("spin")
-	
-func _process(delta):
-	$Path2D/PathFollow2D.offset += follow_speed * delta
