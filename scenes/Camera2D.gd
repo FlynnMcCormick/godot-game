262b2749a1158bf541d35526bcdca3527e5bc313
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-10-25 10:57:37 +1300

    vid13 - 2:00

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -7,6 +7,9 @@ var center = Vector2.ZERO
 
 func _ready():
 	center = get_viewport_rect().size/2
+	target = owner.get_node("Player")
+	position = target.global_position
+	zoom = Vector2(.3,.3)
 
 func _process(delta):
 	if Input.is_action_just_pressed("zoom"):
@@ -18,9 +21,13 @@ func _process(delta):
 			zoomed = true
 
 	if zoomed:
-		zoom = zoom.move_toward(Vector2(0.3,0.3), .03)
-		position = position.move_toward(target.global_position,20)
+		position = target.global_position
+		zoom = Vector2(.3,.3)
+		#zoom = zoom.move_toward(Vector2(0.3,0.3), .03)
+		#position = position.move_toward(target.global_position,20)
 		
 	else:
-		zoom = zoom.move_toward(Vector2(1,1), 0.03)
-		position = position.move_toward(center,20)
+		position = center
+		zoom = Vector2(1,1)
+		#zoom = zoom.move_toward(Vector2(1,1), 0.03)
+		#position = position.move_toward(center,20)
