a05d65c71fd041d4e3524c313aa1637508bb69ad
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-10-28 14:58:42 +1300

    just have to find music

--- scripts/sound_player.gd
+++ scripts/sound_player.gd
@@ -1,5 +1,39 @@
 extends Node
 
+onready var music = AudioStreamPlayer.new()
 
+var music_tracks = {
+	"main":,
+	"title_track":,
+		}
+
+var sound_effects = {
+	"jump":,
+      
+      
+      
+}
+
+var music_db = 1
+var sound_db = 1
+
+func change_music_db(val):
+	 music_db = linear2db(val)
+      
+func change_sound_db(val):
+	sound_db = linear2db(val)
+      
 func _ready():
-	pass 
+	music.stream = load(music_tracks["main"])
+	add_child(music)
+	music.play()
+
+
+
+func play_sound_effect(sfx):
+	var sound = AudioStreamPlayer.new()
+	sound.stream = load(sound_effects[sfx])
+	add_child(sound)
+	sound.play()
+	yield(sound,"finished")
+	sound.queue_free()
